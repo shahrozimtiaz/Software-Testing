4e1f4ef7fa92f6f21b4cb63017bd29c6297b7147
Shahroz Imtiaz <shahrozimtiaz07@gmail.com>
2019-11-23 01:27:09 -0500

    first travis commit

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class ViewTests(TestCase):
+
+    def test_view_login(self):
+        username = 'test_user'
+        password = 'password'
+        user = User.objects.create_user(username=username, password=password)
+        self.assertTrue(User.objects.filter(username=username).exists())
